b2f110d70f14c8f69a862f2fa6ad17eeb4f92800
josdem <joseluis.delacruz@gmail.com>
2018-03-25 13:46:57 -0600

    Formatting code

--- src/main/java/com/jos/dem/webflux/repository/PersonRepository.java
+++ src/main/java/com/jos/dem/webflux/repository/PersonRepository.java
@@ -1,10 +1,7 @@
 package com.jos.dem.webflux.repository;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
 import reactor.core.publisher.Flux;
-
 import com.jos.dem.webflux.model.Person;
 
-public interface PersonRepository extends ReactiveMongoRepository<Person, String> {
-}
+public interface PersonRepository extends ReactiveMongoRepository<Person, String> {}
