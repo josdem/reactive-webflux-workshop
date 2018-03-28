52e5db43ac8fe859dbeadff9e4518eb1b54543d9
josdem <joseluis.delacruz@gmail.com>
2018-03-27 19:21:56 -0600

    Creating client project

---
+++ client/src/main/java/com/jos/dem/webflux/PersonApplication.java
@@ -0,0 +1,12 @@
+package com.jos.dem.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonApplication.class, args);
+	}
+}
