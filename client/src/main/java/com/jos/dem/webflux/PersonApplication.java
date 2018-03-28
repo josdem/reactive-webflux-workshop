a982dcdb451549e32e6031f7bdc959f1100abd6a
josdem <joseluis.delacruz@gmail.com>
2018-03-27 19:29:23 -0600

    Adding hello world concept to the client

--- client/src/main/java/com/jos/dem/webflux/PersonApplication.java
+++ client/src/main/java/com/jos/dem/webflux/PersonApplication.java
@@ -1,5 +1,7 @@
 package com.jos.dem.webflux;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,4 +11,12 @@ public class PersonApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PersonApplication.class, args);
 	}
+
+  @Bean
+  CommandLineRunner run(){
+    return args -> {
+      System.out.println("Hello World!");
+    };
+  }
+
 }
