c1f576f61e8b8fc949422e9c0e1996f0db698a45
josdem <joseluis.delacruz@gmail.com>
2018-11-01 09:48:02 -0400

    Adding required annotation to do serlialization

--- client/src/main/java/com/jos/dem/webflux/model/Person.java
+++ client/src/main/java/com/jos/dem/webflux/model/Person.java
@@ -4,11 +4,13 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
 @Data
 @Document
 @AllArgsConstructor
+@NoArgsConstructor
 public class Person {
 
   @Id
