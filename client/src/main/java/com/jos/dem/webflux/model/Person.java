7eb005173763a24dd58b5976cb4583dcc9c13ef1
josdem <joseluis.delacruz@gmail.com>
2019-05-29 20:23:08 -0400

    Removing unused annotation

--- client/src/main/java/com/jos/dem/webflux/model/Person.java
+++ client/src/main/java/com/jos/dem/webflux/model/Person.java
@@ -1,20 +1,10 @@
 package com.jos.dem.webflux.model;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 @Data
-@Document
-@NoArgsConstructor
-@AllArgsConstructor
 public class Person {
 
-  @Id
-  private String uuid;
   private String nickname;
   private String email;
 
