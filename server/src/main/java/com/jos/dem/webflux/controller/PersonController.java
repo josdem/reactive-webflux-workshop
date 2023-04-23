5bab40b18beec97bf3b94717d2088795df841452
josdem <joseluis.delacruz@gmail.com>
2023-04-23 07:23:07 -0400

    Using Lombok to inject dependencies

--- server/src/main/java/com/jos/dem/webflux/controller/PersonController.java
+++ server/src/main/java/com/jos/dem/webflux/controller/PersonController.java
@@ -3,10 +3,11 @@ package com.jos.dem.webflux.controller;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Flux;
 
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import com.jos.dem.webflux.model.Person;
 import com.jos.dem.webflux.repository.PersonRepository;
@@ -15,10 +16,10 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class PersonController {
 
-    @Autowired
-    private PersonRepository personRepository;
+    private final PersonRepository personRepository;
 
     @GetMapping("/persons")
     public Flux<Person> findAll() {
